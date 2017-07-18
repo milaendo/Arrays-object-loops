338531678624d5c754d1911a79f99e7e91dda515
Mila Endo <milal.alim@gmail.com>
2017-07-18 14:25:21 -0700

    redid num8

--- script.js
+++ script.js
@@ -113,6 +113,15 @@ let first = [ "blink", "stand", "glasses", "chair", "numinous", "adjacent", "bra
 let second = [ "think", "stand", "cheese", "break", "numinous", "mouse", "close", "toe" ];
 
 function union(){
-    let same = [];
+    let same = []
+    for (let i = 0; i < first.length; i++) {
+      for (let j = 0; j < second.length; j++) {
+        if (first[i] === second[j]) {
+          same.push (first[i])
+        }
+      }
+    }
+    return same 
+
     // Your answer here:
 }
